89c5403c460692eae95d56b7520f73916f6afff0
FilipeMallmann <filipe.pm@gmail.com>
2023-04-04 20:33:55 +0000

    Add project files.

---
+++ StreamApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace StreamApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async IAsyncEnumerable<WeatherForecast> Get([EnumeratorCancellation] CancellationToken ct)
+        {
+            int index = 0;
+            while (!ct.IsCancellationRequested)
+            {
+                await Task.Delay(200);
+                yield return new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index++),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                };
+            }
+        }
+    }
+}
